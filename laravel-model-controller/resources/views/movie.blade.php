717a9ff498de27009c6a71f962356d64d10061a6
Andrea Guidotti <andrea19912014@gmail.com>
2022-07-14 18:48:42 +0200

    style

--- laravel-model-controller/resources/views/movie.blade.php
+++ laravel-model-controller/resources/views/movie.blade.php
@@ -4,13 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Laravel Model Controller</title>
 </head>
-<body>
-    <ul>
+<body style="background: #1e2d3b">
+    <ul style="display: flex; gap: 30px">
         @foreach ($movies as $movie)
-            <li>
-                {{ $movie }}
+            <li style="list-style-type: none; text-align: center; color: #ffffff">
+                <h2 style="color: #f9faf9; text-decoration: underline">
+                    Title
+                </h2>
+                <big>
+                    {{ $movie->title }} - {{ $movie->original_title }}
+                </big><br>
+
+                <small>
+                    {{ $movie->nationality }}
+                </small><br>
+
+                <h2 style="color: #f9faf9; text-decoration: underline">
+                    Release
+                </h2>
+                <big>
+                    {{ $movie->date }}
+                </big>
+
+                <h3 style="text-decoration: underline">
+                    Vote
+                </h3>
+                <big style="color: #a4a4a4; font-style: italic">
+                    {{ $movie->vote }}
+                </big><br>
             </li>
         @endforeach
     </ul>
